7675f4bb531c962c8ed1c018cfc21c474b8cdee9
Pearsteam <93873756+Birnendampf@users.noreply.github.com>
2026-02-12 21:08:17 +0100

    better documentation

--- src/nbtcompare/__init__.py
+++ src/nbtcompare/__init__.py
@@ -1,1 +1,5 @@
+"""A simple package for comparing NBT Compound Tags.
+
+A pure python reference implementation of this package is available at [TODO: LINK]
+"""
 from nbtcompare._core import compare
